84a9cae0c9ca40942f798549c8c4cb1bedc02c86
Game-M2096 <ringo3738@gmail.com>
2024-10-26 15:16:22 +0900

    배경 움직임 구현(미완성)

--- main.py
+++ main.py
@@ -1,30 +1,27 @@
 import pygame, sys
-from pygame.locals import*
+from pygame.locals import *
 
 pygame.init()
 
 # 화면 크기 설정
-width = 800
-height = 600
+width, height = 800, 600
 screen = pygame.display.set_mode((width, height))
 
-# 캐릭터 위치와 크기 설정
-# TODO - r1이라는 변수명을 조금 더 의미있게 지어주세요. dictionary로 만들어서 체계적으로 관리해주세요 (spaceshooter 참고하면 됩니다.)
-r1 = pygame.Rect(0, height - 170, 60, 100)
-
-# 배경 이미지 불러오기
-bgImage = pygame.image.load('tsb bg1.jpeg')
-bgImage = pygame.transform.scale(bgImage, (width, height))
-
-# 캐릭터 이미지 불러오기
-right = pygame.image.load('pixel_k-removebg-preview.png')
-right = pygame.transform.scale(right, (60, 100))
-left = pygame.transform.flip(right, True, False)
-knight = right
-
-# 점프 상태 변수
-isJump = False
-jumpStep = 4
+# 캐릭터 데이터 관리
+knight = {
+    'rect': pygame.Rect(0, height - 165, 60, 100),
+    'image_right': pygame.transform.scale(pygame.image.load('pixel_k-removebg-preview.png'), (60,100)),
+    'image_left': None,
+    'vl': 5,
+    'is_jumping': False,
+    'jump_step': 4
+}
+knight['image_left'] = pygame.transform.flip(knight['image_right'], True, False)
+knight['image'] = knight['image_right']
+# 배경 이미지 설정
+bg_image = pygame.image.load('tsb bg1.jpeg')
+bg_image = pygame.transform.scale(bg_image, (width, height))
+backX, backX2 = 0, width  # 배경 두 장을 이어 붙여 스크롤 효과 구현
 
 # 게임 루프
 while True:
@@ -37,30 +34,47 @@ while True:
             sys.exit()
 
     # 키 입력 처리
-    keyInput = pygame.key.get_pressed()
-    if keyInput[K_LEFT] and r1.left > 0:
-        r1.left -= 10
-        knight = left
-    if keyInput[K_RIGHT] and r1.right < width:
-        r1.left += 10
-        knight = right
+    key_input = pygame.key.get_pressed()
+    if key_input[K_LEFT] and knight['rect'].left > 0:
+        backX += knight['vl']
+        backX2 += knight['vl']
+        knight['rect'].left -= knight['vl']
+        knight['image'] = knight['image_left']
 
-    if not isJump:  # 점프 중이 아닐 때만 점프 가능
-        if keyInput[K_SPACE]:
-            isJump = True
-    else:  # 점프 중일 때
-        if jumpStep >= -4:
-            r1.top -= jumpStep * abs(jumpStep) * 5
-            jumpStep -= 1
+    if key_input[K_RIGHT] and knight['rect'].right < width:
+        backX -= knight['vl']
+        backX2 -= knight['vl']
+        knight['rect'].right += knight['vl']
+        knight['image'] = knight['image_right']
+
+    # 점프 처리
+    if not knight['is_jumping']:
+        if key_input[K_SPACE]:
+            knight['is_jumping'] = True
+    else:
+        if knight['jump_step'] >= -4:
+            knight['rect'].top -= knight['jump_step'] * abs(knight['jump_step']) * 5
+            knight['jump_step'] -= 1
         else:
-            isJump = False
-            jumpStep = 4
+            knight['is_jumping'] = False
+            knight['jump_step'] = 4
+
+    # 배경 위치 조정 (스크롤 효과)
+    if backX < -width:
+        backX = width
+    if backX2 < -width:
+        backX2 = width
+    if backX > 0:
+        backX = -width
+    if backX2 > 0:
+        backX2 = -width
 
     # 배경 그리기 (화면 지우기)
-    screen.blit(bgImage, (0, 0))
+    screen.blit(bg_image, (backX, 0))
+    screen.blit(bg_image, (backX2, 0))
 
     # 캐릭터 그리기
-    screen.blit(knight, r1)
+    screen.blit(knight['image'], knight['rect'].topleft)
 
     # 화면 업데이트
     pygame.display.update()
